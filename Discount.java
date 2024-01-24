9098070bfa3cd25ba133d49fbb9c8590d3de99ad
Alexander-Anischenko <156853009+Alexander-Anischenko@users.noreply.github.com>
2024-01-24 17:35:59 +0100

    Add files via upload

---
+++ Discount.java
@@ -0,0 +1,13 @@
+package lesson3;
+public class Discount {
+    public static void main(String[] args) {
+        // Create new variable product A and B
+        int a = 1000;
+        int b = 500;
+        // Make sum
+        int sum = a + b;
+        // Make discount 10%
+        int discount = (sum / 10) * 9;
+        System.out.println(discount);
+    }
+}
